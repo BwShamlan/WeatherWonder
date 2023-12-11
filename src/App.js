39f494fcab6c5472ec43dfd36851d37d00855d66
BwShamlan <BMOHAMMEDSHAMLAN@stu.kau.edu.sa>
2023-12-12 01:20:22 +0300

    Uploaded Reac app for weather

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Home";
+import Weather from "./components/Weather";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <h1>Weather Wonder</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/weather">Weather</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <hr />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/weather" element={<Weather />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Home() {
+  return <h2>Welcome to Weather Wonder! Find current weather conditions.</h2>;
+}
+
+export default Home;
